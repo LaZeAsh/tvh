3ea8583b3617b7e0b2f5c2a59e02c4714f333034
LaZeAsh <ayushrgarg@gmail.com>
2023-07-09 07:22:44 -0700

    Add Google Calendar integration

--- message.py
+++ message.py
@@ -4,7 +4,13 @@ from twilio.rest import Client
 from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 final_time=""
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly', 'https://www.googleapis.com/auth/calendar', 'https://www.googleapis.com/auth/calendar.events']
 @app.route('/gather-handler', methods=['POST'])
 def gather_handler():
     account_sid = os.environ['TWILIO_ACCOUNT_SID']
@@ -24,8 +30,9 @@ def gather_handler():
                      from_='+18336202568',
                      to='+19257388240'
                  )
+        main()
     elif digits == '2':
-        response_text=f'<Response><Say>Press 1 for 6:00 PM, Press 2 for 8:00 PM, or Press 3 for none of the above</Say><Gather numDigits="1" action="https://6f83-50-211-203-150.ngrok-free.app/time-handle" method="POST"></Gather></Response>'    # Generate the TwiML response to send back to Twilio
+        response_text=f'<Response><Say>Press 1 for 6:00 PM, Press 2 for 8:00 PM, or Press 3 for none of the above</Say><Gather numDigits="1" action="https://a6fe-50-211-203-150.ngrok-free.app/time-handle" method="POST"></Gather></Response>'    # Generate the TwiML response to send back to Twilio
     twiml_response = f"""{response_text}"""
     return twiml_response, 200, {'Content-Type': 'application/xml'}
 
@@ -68,6 +75,86 @@ def time_handler():
         </Response>
     """
     return twiml_response, 200, {'Content-Type': 'application/xml'}
+
+def main():
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build('calendar', 'v3', credentials=creds)
+        # calendar = {
+        #     'summary': 'calendarSummary',
+        #     'timeZone': 'America/Los_Angeles',
+        #     'description': 'This is an event',
+
+        # }
+
+        # created_calendar = service.calendars().insert(body=calendar).execute()
+
+                # print (created_calendar['id'])
+        event = {
+        'summary': 'Haircut',
+        'location': 'Pleasanton Barber Shop',
+        'description': '',
+        'start': {
+            'dateTime': '2023-07-09T07:00:00-08:00',
+            'timeZone': 'America/Los_Angeles',
+        },
+        'end': {
+            'dateTime': '2023-07-09T07:00:00-08:00',
+            'timeZone': 'America/Los_Angeles',
+        },
+        'recurrence': [
+            'RRULE:FREQ=DAILY;COUNT=2'
+        ],
+        'reminders': {
+            'useDefault': False,
+            'overrides': [
+            {'method': 'email', 'minutes': 24 * 60},
+            {'method': 'popup', 'minutes': 10},
+            ],
+        },
+        }
+
+        event = service.events().insert(calendarId='primary', body=event).execute()
+        print('Event created: %s' % (event.get('htmlLink')))
+        # # Call the Calendar API
+        # now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+        # print('Getting the upcoming 10 events')
+        # events_result = service.events().list(calendarId='primary', timeMin=now,
+        #                                       maxResults=10, singleEvents=True,
+        #                                       orderBy='startTime').execute()
+        # events = events_result.get('items', [])
+
+        # if not events:
+        #     print('No upcoming events found.')
+        #     return
+
+        # # Prints the start and name of the next 10 events
+        # for event in events:
+        #     start = event['start'].get('dateTime', event['start'].get('date'))
+        #     print(start, event['summary'])
+
+    except HttpError as error:
+        print('An error occurred: %s' % error)
 
 if __name__ == '__main__':
     app.run()
